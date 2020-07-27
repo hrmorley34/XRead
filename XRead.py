eb325cfcafe147705388e41dec5522dc7bf1eb1d
hrmorley34 <hrmorley3.4@gmail.com>
2020-07-27 15:15:33 +0100

    Blackening

--- XRead.py
+++ XRead.py
@@ -6,96 +6,111 @@ import tkinter.filedialog as fd
 import tkinter.messagebox as mb
 import XReadWeb as web
 
-version='1.1.3 +WE'+web.version+''
-xreadInfo='This is XRead Text Editor version '+version+'''.
+version = f"1.1.3 +WE{web.version}"
+xreadInfo = f"""This is XRead Text Editor version {version}.
 This uses Python 3 and Tkinter.
 Created by hrmorley34 on Github.
-https://github.com/hrmorley34/XRead'''
+https://github.com/hrmorley34/XRead"""
+
 
 class AppXRTE:
-    def __init__(self,master):
-        self.file_is_saved=False
-        self.cfile=None
+    def __init__(self, master):
+        self.file_is_saved = False
+        self.cfile = None
         self.master = master
 
         menubar = Menu(root)
 
-        filemenu=Menu(menubar,tearoff=0)
-        filemenu.add_command(label='New',command=self.New,accelerator="Ctrl+N")
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New", command=self.New, accelerator="Ctrl+N")
         master.bind("<Control-Key-N>", self.New)
-        filemenu.add_command(label='Open',command=self.Open,accelerator="Ctrl+O")
+        filemenu.add_command(label="Open", command=self.Open, accelerator="Ctrl+O")
         master.bind("<Control-Key-O>", self.Open)
-        filemenu.add_command(label='Save',command=self.Save,accelerator="Ctrl+S")
+        filemenu.add_command(label="Save", command=self.Save, accelerator="Ctrl+S")
         master.bind("<Control-Key-S>", self.Save)
-        filemenu.add_command(label='Save As...',command=self.Save_as,accelerator="Ctrl+Shift+S")
+        filemenu.add_command(
+            label="Save As...", command=self.Save_as, accelerator="Ctrl+Shift+S"
+        )
         master.bind("<Control-Shift-Key-S>", self.Save_as)
-        filemenu.add_command(label='Quit',command=self.Q,accelerator="Ctrl+Q")
+        filemenu.add_command(label="Quit", command=self.Q, accelerator="Ctrl+Q")
         master.bind("<Control-Key-Q>", self.Q)
-        menubar.add_cascade(label='File',menu=filemenu)
+        menubar.add_cascade(label="File", menu=filemenu)
 
-        webedit=Menu(menubar,tearoff=0)
-        webedit.add_command(label='Import XML',command=self.AppXRWEOpen,accelerator="Ctrl+I")
+        webedit = Menu(menubar, tearoff=0)
+        webedit.add_command(
+            label="Import XML", command=self.AppXRWEOpen, accelerator="Ctrl+I"
+        )
         master.bind("<Control-Key-I>", self.AppXRWEOpen)
-        webedit.add_command(label='Export XML...',command=self.AppXRWESave_as,accelerator="Ctrl+X")
+        webedit.add_command(
+            label="Export XML...", command=self.AppXRWESave_as, accelerator="Ctrl+X"
+        )
         master.bind("<Control-Key-X>", self.AppXRWESave_as)
-        menubar.add_cascade(label='Web Editor',menu=webedit)
+        menubar.add_cascade(label="Web Editor", menu=webedit)
 
-        helpmenu=Menu(menubar,tearoff=0)
-        helpmenu.add_command(label='About XRead',command=self.About,accelerator="Ctrl+A")
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(
+            label="About XRead", command=self.About, accelerator="Ctrl+A"
+        )
         master.bind("<Control-Key-A>", self.About)
-        helpmenu.add_command(label='About Web Editor',command=self.AppXRWEAbout,accelerator="Ctrl+W")
+        helpmenu.add_command(
+            label="About Web Editor", command=self.AppXRWEAbout, accelerator="Ctrl+W"
+        )
         master.bind("<Control-Key-W>", self.AppXRWEAbout)
-        helpmenu.add_command(label='Web Editor Style',command=self.AppXRWEStyle,accelerator="Ctrl+Shift+W")
+        helpmenu.add_command(
+            label="Web Editor Style",
+            command=self.AppXRWEStyle,
+            accelerator="Ctrl+Shift+W",
+        )
         master.bind("<Control-Shift-Key-W>", self.AppXRWEStyle)
-        menubar.add_cascade(label='Help',menu=helpmenu)
+        menubar.add_cascade(label="Help", menu=helpmenu)
 
         master.config(menu=menubar)
         master.bind("<<close-window>>", self.Q)
-        
-        self.text=Text(master)
+
+        self.text = Text(master)
         self.text.pack(expand=YES, fill=BOTH)
 
     def Q(self):
-        if mb.askyesno('Save?','Would you like to save?'):
+        if mb.askyesno("Save?", "Would you like to save?"):
             self.Save()
         exit()
 
     def New(self):
-        if mb.askyesno('Save?','Would you like to save?'):
+        if mb.askyesno("Save?", "Would you like to save?"):
             self.Save()
-        self.file_is_saved=False
-        self.text.delete("1.0",END)
-        self.master.wm_title('XRead Text Editor '+version)
+        self.file_is_saved = False
+        self.text.delete("1.0", END)
+        self.master.wm_title("XRead Text Editor " + version)
 
     def Open(self):
-        if mb.askyesno('Save?','Would you like to save?'):
+        if mb.askyesno("Save?", "Would you like to save?"):
             self.Save()
-        file = fd.askopenfile(mode='r')
-        self.text.delete("1.0",END)
+        file = fd.askopenfile(mode="r")
+        self.text.delete("1.0", END)
         self.cfile = file.name
-        self.master.wm_title('XRead Text Editor - '+self.cfile)
-        self.text.insert(END,file.read())
+        self.master.wm_title("XRead Text Editor - " + self.cfile)
+        self.text.insert(END, file.read())
         file.close()
-        self.file_is_saved=True
+        self.file_is_saved = True
 
     def Save(self):
         if self.file_is_saved:
-            f = open(self.cfile,mode='w')
-            f.write(self.text.get("1.0",END))
+            f = open(self.cfile, mode="w")
+            f.write(self.text.get("1.0", END))
             f.close()
         else:
             self.Save_as()
 
     def Save_as(self):
-        f = fd.asksaveasfile(mode='w')
+        f = fd.asksaveasfile(mode="w")
         self.cfile = f.name
-        self.master.wm_title('XRead Text Editor - '+self.cfile)
-        f.write(self.text.get("1.0",END))
+        self.master.wm_title("XRead Text Editor - " + self.cfile)
+        f.write(self.text.get("1.0", END))
         f.close()
-        self.file_is_saved=True
+        self.file_is_saved = True
 
     def About(self):
-        mb.showinfo('About XRead',xreadInfo)
+        mb.showinfo("About XRead", xreadInfo)
 
     def AppXRWEOpen(self):
         web.AppXRWE.Open(self)
@@ -109,8 +124,9 @@ class AppXRTE:
     def AppXRWEStyle(self):
         web.AppXRWE.Style(self)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     root = Tk()
-    root.wm_title('XRead Text Editor '+version)
+    root.wm_title("XRead Text Editor " + version)
     app = AppXRTE(root)
     root.mainloop()
--- XReadWeb.py
+++ XReadWeb.py
@@ -6,13 +6,13 @@ import tkinter.filedialog as fd
 import tkinter.messagebox as mb
 from xml.etree import ElementTree as ET
 
-version='1.2'
-xreadInfo='This is XRead Web/XML Editor version '+version+'''.
+version = "1.2"
+xreadInfo = f"""This is XRead Web/XML Editor version {version}.
 This uses Python 3 and Tkinter.
 Created by hrmorley34 on Github.
-https://github.com/hrmorley34/XRead'''
+https://github.com/hrmorley34/XRead"""
 
-wewi = '''Writing style:
+wewi = """Writing style:
 Tabs (indentation) represent depth
 into the XML tree. Each line is
 then written to the form:
@@ -22,39 +22,51 @@ tagname '#'  '#'  '#' {}
 Note a double space where the h is:
  '#'HH'#'
 That is it.
-'''
+"""
+
 
 def htmli(xml):
     root = ET.fromstring(xml)
     out = x2i(root)
-    return(out)
+    return out
+
 
 def htmlo(text):
     root = i2x(text)
-    out = ET.tostring(root, encoding='unicode')
-    return(out)
+    out = ET.tostring(root, encoding="unicode")
+    return out
+
 
 def x2i(root):
     if root.text == None:
-        root.text = ''
+        root.text = ""
     if root.tail == None:
-        root.tail = ''
-    ic = str(root.tag) +" '#' "+ str(root.text) +" '#' "+ str(root.tail) +" '#' "+ str(root.attrib)
+        root.tail = ""
+    ic = (
+        str(root.tag)
+        + " '#' "
+        + str(root.text)
+        + " '#' "
+        + str(root.tail)
+        + " '#' "
+        + str(root.attrib)
+    )
     for child in root:
         v = x2i(child)
-        for line in v.split('\n'):
-            ic += '\n\t' + line
-    return(ic)
+        for line in v.split("\n"):
+            ic += "\n\t" + line
+    return ic
+
 
 def i2x(text):
     l = []
     oldx = 0
-    for line in text.split('\n'):
-        if line != '':
+    for line in text.split("\n"):
+        if line != "":
             x = 0
             for char in line:
-                if char == '\t':
-                    x+=1
+                if char == "\t":
+                    x += 1
                 else:
                     break
             oldx = x
@@ -63,69 +75,72 @@ def i2x(text):
     plev = {}
     for i in l:
         if i[0] == 0:
-            plev[0] = ET.Element(i[1],i[2])
+            plev[0] = ET.Element(i[1], i[2])
             plev[0].text = i[3]
             plev[0].tail = i[4]
         else:
-            plev[i[0]] = ET.SubElement(plev[i[0]-1],i[1],i[2])
+            plev[i[0]] = ET.SubElement(plev[i[0] - 1], i[1], i[2])
             plev[i[0]].text = i[3]
             plev[i[0]].tail = i[4]
     root = plev[0]
-    return(root)
+    return root
+
 
 class AppXRWE:
-    def __init__(self,master):
-        self.wfile_is_saved=False
-        self.wcfile=None
+    def __init__(self, master):
+        self.wfile_is_saved = False
+        self.wcfile = None
 
         menubar = Menu(root)
 
-        filemenu=Menu(menubar,tearoff=0)
-        filemenu.add_command(label='New',command=self.New,accelerator="Ctrl+N")
-        filemenu.add_command(label='Open',command=self.Open,accelerator="Ctrl+O")
-        filemenu.add_command(label='Save',command=self.Save,accelerator="Ctrl+S")
-        filemenu.add_command(label='Save As...',command=self.Save_as,accelerator="Ctrl+Shift+S")
-        filemenu.add_command(label='Quit',command=self.Q,accelerator="Ctrl+Q")
-        menubar.add_cascade(label='File',menu=filemenu)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New", command=self.New, accelerator="Ctrl+N")
+        filemenu.add_command(label="Open", command=self.Open, accelerator="Ctrl+O")
+        filemenu.add_command(label="Save", command=self.Save, accelerator="Ctrl+S")
+        filemenu.add_command(
+            label="Save As...", command=self.Save_as, accelerator="Ctrl+Shift+S"
+        )
+        filemenu.add_command(label="Quit", command=self.Q, accelerator="Ctrl+Q")
+        menubar.add_cascade(label="File", menu=filemenu)
 
-        helpmenu=Menu(menubar,tearoff=0)
-        helpmenu.add_command(label='About',command=self.About,accelerator="Ctrl+A")
-        helpmenu.add_command(label='WE Style',command=self.Style,accelerator="Ctrl+W")
-        menubar.add_cascade(label='Help',menu=helpmenu)
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="About", command=self.About, accelerator="Ctrl+A")
+        helpmenu.add_command(label="WE Style", command=self.Style, accelerator="Ctrl+W")
+        menubar.add_cascade(label="Help", menu=helpmenu)
 
         master.config(menu=menubar)
         master.bind("<<close-window>>", self.Q)
-        
-        self.text=Text(master)
+
+        self.text = Text(master)
         self.text.pack(expand=YES, fill=BOTH)
 
     def Q(self):
-        if mb.askyesno('Save?','Would you like to save?'):
+        if mb.askyesno("Save?", "Would you like to save?"):
             self.Save()
         exit()
 
     def New(self):
-        if mb.askyesno('Save?','Would you like to save?'):
+        if mb.askyesno("Save?", "Would you like to save?"):
             self.Save()
-        self.wfile_is_saved=False
-        self.text.delete("1.0",END)
+        self.wfile_is_saved = False
+        self.text.delete("1.0", END)
 
     def Open(self):
-        if mb.askyesno('Save?','Would you like to save?'):
+        if mb.askyesno("Save?", "Would you like to save?"):
             self.Save()
-        file = fd.askopenfile(mode='r')
-        self.text.delete("1.0",END)
+        file = fd.askopenfile(mode="r")
+        self.text.delete("1.0", END)
         self.wcfile = file.name
-        input_ = END,file.read()
+        input_ = END, file.read()
         output = htmli(input_)
         self.text.insert(output)
         file.close()
-        self.wfile_is_saved=True
+        self.wfile_is_saved = True
 
     def Save(self):
         if self.wfile_is_saved:
-            f = open(self.wcfile,mode='w')
-            input_ = self.text.get("1.0",END)
+            f = open(self.wcfile, mode="w")
+            input_ = self.text.get("1.0", END)
             output = htmlo(input_)
             f.write(output)
             f.close()
@@ -133,22 +148,23 @@ class AppXRWE:
             self.Save_as()
 
     def Save_as(self):
-        f = fd.asksaveasfile(mode='w')
+        f = fd.asksaveasfile(mode="w")
         self.wcfile = f.name
-        input_ = self.text.get("1.0",END)
+        input_ = self.text.get("1.0", END)
         output = htmlo(input_)
         f.write(output)
         f.close()
-        self.wfile_is_saved=True
+        self.wfile_is_saved = True
 
     def About(self):
-        mb.showinfo('About XRead WE',xreadInfo)
+        mb.showinfo("About XRead WE", xreadInfo)
 
     def Style(self):
-        mb.showinfo('XRead WE Writing Style',wewi)
+        mb.showinfo("XRead WE Writing Style", wewi)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     root = Tk()
-    root.wm_title('XRead Web Editor')
+    root.wm_title("XRead Web Editor")
     app = AppXRWE(root)
     root.mainloop()
